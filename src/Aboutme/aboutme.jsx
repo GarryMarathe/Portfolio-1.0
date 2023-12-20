543a2db7aacbc77b443dcb78413d23c1c3b6f63d
GauravMarathe <gauravmarathe6468@gmail.com>
2023-12-20 14:16:44 +0530

    resume click

--- src/Aboutme/aboutme.jsx
+++ src/Aboutme/aboutme.jsx
@@ -22,31 +22,42 @@ const AboutSection = () => {
               </div>
               <div className="right-section">
                   <div className="column-1">
-                      <h2>I'm Gaurav</h2>
+                      <h2>I'm  <span>Gaurav</span>
+                      </h2>
                   </div>
-                  <div className="column-2">
-                      <p>Web Developer</p>
-                  </div>
+                  
                   <div className="column-3">
                       <p>
-                          Hello! I'm [Your Name], a passionate [Your Profession] based in [Your Location].
-                          [Add more details about yourself here.]
+                      I'm a fourth-year IT undergrad with a passion for exploring new technologies. Web development and UI/UX design have captured my interest, so equipped with Web Development skills. I'm also an avid gamer and enjoy immersing myself in virtual worlds. My hardworking nature drives me to tackle any challenge head-on and ensure tasks are completed with excellence. With a strong commitment to both my studies and personal interests, I'm excited to apply my skills, dedication, and enthusiasm to make a meaningful impact in the field.
                       </p>
                       
                   </div>
                   <div className="boxes-container">
-                        <div className="box">Box 1 Text</div>
-                        <div className="box">Box 2 Text</div>
-                        <div className="box">Box 3 Text</div>
+                  <div className="box-1">
+                             <img src="/icons/title.png" alt="Role Icon" />
+                            <h3>Role</h3>
+                            <p>Full Stack Developer</p>
+                        </div>
+                        <div className="box-1">
+                            <img src="/icons/graduation-cap.png" alt="Education Icon" />
+                            <h3>Education</h3>
+                            <p>BE Undergrad IT</p>
+                        </div>
+                        <div className="box-3">
+
+                   
+                         <button>
+                         <img src="/icons/file.png" alt="download" />
+                           <h4>Resume</h4> 
+                           <p>Download</p>
+                        </button>
+            
+
+                    </div>
+                
                     </div>
 
-                    <div className="download-button">
                     
-                        <button>
-                         <img src="/icons/file.png" alt="download" />
-                            Download
-                        </button>
-                    </div>
               </div>
           </div>
       </div>
