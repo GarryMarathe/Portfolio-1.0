53ec975ccd22fc790a3f9dd2a71d82480283fefe
GauravMarathe <gauravmarathe6468@gmail.com>
2023-12-23 13:55:59 +0530

    skills section

--- src/Aboutme/aboutme.jsx
+++ src/Aboutme/aboutme.jsx
@@ -6,35 +6,35 @@ import { FaDownload } from 'react-icons/fa'; // Import the download icon from re
 
 const AboutSection = () => {
 
-          
+
     return (
 
 
 
-      <div className="about-section">
-          <div className="about-me-text">
-              <h1>About Me</h1>
-              <hr className="line" />
-          </div>
-          <div className="content-section">
-              <div className="left-section">
-                  <img src="/icons/Profile.jpeg" alt="Profile" />
-              </div>
-              <div className="right-section">
-                  <div className="column-1">
-                      <h2>I'm  <span>Gaurav</span>
-                      </h2>
-                  </div>
-                  
-                  <div className="column-3">
-                      <p>
-                      I'm a fourth-year IT undergrad with a passion for exploring new technologies. Web development and UI/UX design have captured my interest, so equipped with Web Development skills. I'm also an avid gamer and enjoy immersing myself in virtual worlds. My hardworking nature drives me to tackle any challenge head-on and ensure tasks are completed with excellence. With a strong commitment to both my studies and personal interests, I'm excited to apply my skills, dedication, and enthusiasm to make a meaningful impact in the field.
-                      </p>
-                      
-                  </div>
-                  <div className="boxes-container">
-                  <div className="box-1">
-                             <img src="/icons/title.png" alt="Role Icon" />
+        <div className="about-section">
+            <div className="about-me-text">
+                <h1>About Me</h1>
+                <hr className="line" />
+            </div>
+            <div className="content-section">
+                <div className="left-section">
+                    <img src="/icons/Profile.jpeg" alt="Profile" />
+                </div>
+                <div className="right-section">
+                    <div className="column-1">
+                        <h2>I'm  <span>Gaurav</span>
+                        </h2>
+                    </div>
+
+                    <div className="column-3">
+                        <p>
+                            I'm a fourth-year IT undergrad with a passion for exploring new technologies. Web development and UI/UX design have captured my interest, so equipped with Web Development skills. I'm also an avid gamer and enjoy immersing myself in virtual worlds. My hardworking nature drives me to tackle any challenge head-on and ensure tasks are completed with excellence. With a strong commitment to both my studies and personal interests, I'm excited to apply my skills, dedication, and enthusiasm to make a meaningful impact in the field.
+                        </p>
+
+                    </div>
+                    <div className="boxes-container">
+                        <div className="box-1">
+                            <img src="/icons/title.png" alt="Role Icon" />
                             <h3>Role</h3>
                             <p>Full Stack Developer</p>
                         </div>
@@ -45,27 +45,27 @@ const AboutSection = () => {
                         </div>
                         <div className="box-3">
 
-                        <a href="/resumes/resume.pdf" download>
-                        
-                         <img src="/icons/file.png" alt="download" />
-                           <h3>Resume</h3> 
-                           <p>Download</p>
+                            <a href="/resumes/resume.pdf" download>
 
-                         
-                        </ a>
-            
+                                <img src="/icons/file.png" alt="download" />
+                                <h3>Resume</h3>
+                                <p>Download</p>
+
+
+                            </ a>
+
+
+                        </div>
 
                     </div>
-                
-                    </div>
-
-                    
-              </div>
-          </div>
-      </div>
 
 
-  );
+                </div>
+            </div>
+        </div>
+
+
+    );
 }
 
 export default AboutSection;
--- src/Top/top.jsx
+++ src/Top/top.jsx
@@ -3,7 +3,7 @@ import './top.css';
 import backgroundImage from '../safar-safarov-MSN8TFhJ0is-unsplash.jpg';
 import { init } from 'ityped';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 
@@ -14,7 +14,7 @@ const Top = () => {
   const jobTitleRef = useRef(null); // Reference for the job title element
 
   useEffect(() => {
-    
+
     const handleScroll = () => {
       const navbar = document.querySelector('.navbar');
       if (window.scrollY > 100) {
@@ -32,7 +32,7 @@ const Top = () => {
       typeSpeed: 80,
       backSpeed: 60,
       backDelay: 1500,
-    
+
       // startDelay: 500, 
     });
 
@@ -46,102 +46,102 @@ const Top = () => {
     setShowMenu(!showMenu);
   };
 
-  
+
 
   return (
     <div className="background-container">
       <img src={backgroundImage} alt="Background" className="background-image" />
       <nav className="navbar">
-      <div className="navbar-left">
-           <a href="/" className="portfolio-link">
-             Portfolio
-           </a>
-         </div>
-         <div className={`menu-icon ${showMenu ? 'active' : ''}`} onClick={toggleMenu}>
-           <div className="bar"></div>
-           <div className="bar"></div>
-           <div className="bar"></div>
-         </div>
-         <div className={`navbar-center ${showMenu ? 'active' : ''}`}>
-           <div className="close-icon" onClick={toggleMenu}>
-             &times;
-           </div>
-           <a href="/" onClick={toggleMenu}>
-             Home
-           </a>
-           <a href="/" onClick={toggleMenu}>
-             Resume
+        <div className="navbar-left">
+          <a href="/" className="portfolio-link">
+            Portfolio
           </a>
-           <a href="/" onClick={toggleMenu}>
-             Projects
-           </a>
-           <a href="/" onClick={toggleMenu}>
-             About Me
-           </a>
-           <a href="/" onClick={toggleMenu}>
-             Contact
-           </a>
-         </div>
-       
+        </div>
+        <div className={`menu-icon ${showMenu ? 'active' : ''}`} onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <div className={`navbar-center ${showMenu ? 'active' : ''}`}>
+          <div className="close-icon" onClick={toggleMenu}>
+            &times;
+          </div>
+          <a href="/" onClick={toggleMenu}>
+            Home
+          </a>
+          <a href="/" onClick={toggleMenu}>
+            Resume
+          </a>
+          <a href="/" onClick={toggleMenu}>
+            Projects
+          </a>
+          <a href="/" onClick={toggleMenu}>
+            About Me
+          </a>
+          <a href="/" onClick={toggleMenu}>
+            Contact
+          </a>
+        </div>
+
         {/* Your existing navbar content */}
 
       </nav>
       <div className="content-container">
         <div className="left">
-        <div className="section-name">
-          <h1>
-            Hi, I'm <span >Gaurav Marathe</span>
-          </h1>
-        </div>
-        <div className="job-title-column">
-          <h2>
-          <span className="job-title-text">a</span>
-            <span ref={jobTitleRef}></span>
-          </h2>
-        </div>
-        <div className="section">
-          <p>
-          I am a passionate MERN stack developer and machine learning enthusiast. With a strong foundation in web development and a keen eye for data, I create innovative solutions that bridge technology and user experience.
-          </p>
-        </div>
+          <div className="section-name">
+            <h1>
+              Hi, I'm <span >Gaurav Marathe</span>
+            </h1>
+          </div>
+          <div className="job-title-column">
+            <h2>
+              <span className="job-title-text">a</span>
+              <span ref={jobTitleRef}></span>
+            </h2>
+          </div>
+          <div className="section">
+            <p>
+              I am a passionate MERN stack developer and machine learning enthusiast. With a strong foundation in web development and a keen eye for data, I create innovative solutions that bridge technology and user experience.
+            </p>
+          </div>
         </div>
 
 
         <div className="social-link">
-          
+
           <div className="find">
-          <h3>FIND ME ON</h3>
+            <h3>FIND ME ON</h3>
           </div>
-          
+
 
           <div className="link">
-          <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" className='icon-link'>
-            <FontAwesomeIcon icon = {faGithub} size ='1x' className='ficons'/>
-          </a>
-          <a href="mailto:your.email@gmail.com" target="_blank" rel="noopener noreferrer0" className='icon-link'>
-          <FontAwesomeIcon icon = {faEnvelope} size ='1x' className='ficons'/>
-          </a>
-          <a href="https://www.linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer" className='icon-link'>
-          <FontAwesomeIcon icon = {faLinkedin} size ='1x' className='ficons'/>
-          </a>
+            <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" className='icon-link'>
+              <FontAwesomeIcon icon={faGithub} size='1x' className='ficons' />
+            </a>
+            <a href="mailto:your.email@gmail.com" target="_blank" rel="noopener noreferrer0" className='icon-link'>
+              <FontAwesomeIcon icon={faEnvelope} size='1x' className='ficons' />
+            </a>
+            <a href="https://www.linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer" className='icon-link'>
+              <FontAwesomeIcon icon={faLinkedin} size='1x' className='ficons' />
+            </a>
           </div>
 
           <div className="stack">
-          <h3>WEB STACK</h3>
-          <div className="stack-icons">
-          <div className="icon-link">
-            <img src="/icons/icons8-react (2).svg" alt="React" className="icon-svg" />
-          </div>
-          <div className="icon-link">
-            <img src="/icons/mongodb-white.svg" alt="MongoDB" className="icon-svg" />
-          </div>
-          <div className="icon-link">
-            <img src="/icons/pocoo_flask-icon.svg" alt="Flask" className="icon-svgg" />
-          </div>
-          <div className="icon-link">
-            <img src="/icons/icons8-javascript.svg" alt="Express" className="icon-svg" />
-          </div>
-        </div>
+            <h3>WEB STACK</h3>
+            <div className="stack-icons">
+              <div className="icon-link">
+                <img src="/icons/icons8-react (2).svg" alt="React" className="icon-svg" />
+              </div>
+              <div className="icon-link">
+                <img src="/icons/mongodb-white.svg" alt="MongoDB" className="icon-svg" />
+              </div>
+              <div className="icon-link">
+                <img src="/icons/pocoo_flask-icon.svg" alt="Flask" className="icon-svgg" />
+              </div>
+              <div className="icon-link">
+                <img src="/icons/icons8-javascript.svg" alt="Express" className="icon-svg" />
+              </div>
+            </div>
 
 
           </div>
